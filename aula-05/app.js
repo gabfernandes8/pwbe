e227648d8c0005c3eaa6f3d7ed42a4da68398ead
GABRIELA FERNANDES CAVALCANTI <23111725@CFP127ALUNO.LOCAL>
2023-09-01 12:23:04 -0300

    aa

---
+++ aula-05/app.js
@@ -0,0 +1,6 @@
+/**********************************************************
+ * Objetivo: realizar o cálculo de uma determinada tabuada
+ * Data: 01/09/2023
+ * Autor: Gabriela Fernandes
+ * Versão 1.0
+ *********************************************************/
---
+++ aula-05/modulo/calculoTabuada.js
@@ -0,0 +1,47 @@
+/***********************************************************
+ * Objetivo: arquivo de funções para calcular uma tabuada
+ * Data: 01/09/2023
+ * Autor: Gabriela Fernandes
+ * versão: 1.0
+ **********************************************************/
+
+// função para calcular uma tabuada
+const calcularTabuada = function(valorTabuada, contadorInicial, contadorFinal){
+    // entradas de dados que chegam pelos argumentos 
+    let tabuada = String(valorTabuada).replace(',', '.')
+    let contInicial = String(contadorInicial).replace(',', '.')
+    let contFinal = String(contadorFinal).replace(',', '.')
+
+    let resultado
+    let status = false
+
+    // validação para organizar valores (menor e maior)
+    if(contFinal < contInicial){
+        contFinal = contInicial
+        contInicial = contFinal
+    }
+
+    if (tabuada == '' || contInicial == '' || contFinal == '') {
+        console.log('ERRO: É obrigatório a entrada de todos os valores.')
+    } else if(isNaN(tabuada) || isNaN(contInicial) || isNaN(contFinal)){
+        console.log('ERRO: É obrigatório a entrada somente de números')
+    } else {
+        // converte as variáveis em números
+        contInicial = Number(contInicial)
+        contFinal = Number(contFinal)
+        tabuada = Number(tabuada)
+
+        // exemplo usando while
+        while (contInicial <= contFinal){
+            resultado = tabuada * contInicial
+            console.log(tabuada + ' x ' + contInicial + ' = ' + resultado)
+
+            contInicial++
+            status = true
+        }
+    }
+
+    return status
+}
+
+console.log(calcularTabuada(8,1,10))
