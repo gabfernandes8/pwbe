3d2a68e5ef582233aeec0856b19d638f2c74cf66
GABRIELA FERNANDES CAVALCANTI <23111725@CFP127ALUNO.LOCAL>
2023-11-10 10:09:13 -0300

    aula 07

---
+++ atividade-estados/app.js
@@ -0,0 +1,41 @@
+/****************************************************
+ * OBJETIVO: criar uma api para responder dados de Estados e Cidades
+ * DATA: 10/11/2023
+ * AUTOR: Gabriela Fernandes
+ * VERSÃO: 1.0
+ ***************************************************/
+
+/*****************************************************
+ * Instalações das dependencias para criações de API
+ *      - express (npm install express --save)
+ *          => dependencia do node para auxiliar na criação da api
+ * 
+ *      - cors (npm install cors --save)
+ *          => dependencia para manipular recursos de acesso, permissões, etc, da api
+ * 
+ *      - body-parser (npm install body-parser --save)
+ *          => dependencia para auxiliar na chegada de dados da api
+ ****************************************************/
+
+// import das bibliotecass do projeto
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+// cria um objeto app tendo como referência a classe do express
+const app = express()
+
+/**
+ * request = receber dados
+ * response = devolve dados
+ **/
+
+// função para configurar as permissõe do cors
+app.use((request, response, next) => {
+
+    // configura quem poderá fazer requisiçõs na api (* - libera para todos | ip restringe o acesso)
+    response.header('Access-Control-Allow-Origin', '*')
+
+    // configura os métodos que poderão ser utilizados na api (GET, POST, PUT e DELETE)
+    response.header('Access-Control-Allow-Methods', 'GET')
+})
